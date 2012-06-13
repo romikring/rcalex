629cbcf1f211c7f8ffe29259fa370c2069d016cf
Roman Gabrusenok <romikring@gmail.com>
2012-06-13 19:03:09 +0300

    configure index.php file

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
+//Doc root path
+defined('DOCUMENT_ROOT')
+    || define('DOCUMENT_ROOT', realpath(dirname(__FILE__)));
+
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
@@ -16,6 +20,10 @@ set_include_path(implode(PATH_SEPARATOR, array(
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
+require_once 'Zend/Loader/Autoloader.php';
+
+$autoloader = Zend_Loader_Autoloader::getInstance();
+$autoloader->registerNamespace('Rabotal_');
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
@@ -23,4 +31,4 @@ $application = new Zend_Application(
     APPLICATION_PATH . '/configs/application.ini'
 );
 $application->bootstrap()
-            ->run();
+    ->run();
